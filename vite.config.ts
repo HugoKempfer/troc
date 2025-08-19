74f3d91ff652ef21531474eb41ba5fbfbefb44a1
Hugo Kempfer <hugkempf@gmail.com>
2025-08-20 02:23:30 +0300

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
             src: 'maskable_icon_x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           }
         ]
       },
