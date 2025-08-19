7eddc1896cc84597faa612e328c5f2ecbd91b3bf
Hugo Kempfer <hugkempf@gmail.com>
2025-08-19 03:49:43 +0300

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  base: '/troc/',
+  plugins: [vue(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Troc - Currency Converter',
+        short_name: 'Troc',
+        description: 'A simple open-source currency converter.',
+        theme_color: '#ffffff',
+        icons: []
+      },
+    })
+  ],
 })
